42c787f20de6a5039953aace290841ac9a9d039a
Tunnelblick <larsss1024@gmail.com>
2022-07-24 16:08:00 +0200

    Make collab status editable #9

---
+++ database.clear.bat
@@ -0,0 +1,5 @@
+docker stop postgresql || true
+docker rm postgresql || true
+docker run --name postgresql -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres -p 5432:5432 -v /data:/var/lib/postgresql/data -d postgres
+rm -rf /var/lib/postgresql/data
+npx prisma migrate dev
