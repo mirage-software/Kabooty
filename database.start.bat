0214daf787c9add9def6f925a3049fce5a9eea2d
Tunnelblick <larsss1024@gmail.com>
2022-07-24 16:08:58 +0200

    remove not needed files

--- database.start.bat
+++
@@ -1,4 +0,0 @@
-docker stop postgresql
-docker rm postgresql
-docker run --name postgresql -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres -p 5432:5432 -v /data:/var/lib/postgresql/data -d postgres
-npx prisma migrate dev
