5b412afaa9028934b632e27306425854d0cb5552
Aris <64918822+Arisamiga@users.noreply.github.com>
2022-07-30 16:08:58 +0100

    Formatted index.ts

--- src/routes/api/collabs/[id]/picks/[pick_id]/index.ts
+++ src/routes/api/collabs/[id]/picks/[pick_id]/index.ts
@@ -199,7 +199,7 @@ export const del: RequestHandler = async ({ request, params }) => {
 	const decodedUser = Jwt.decode(cookies['user_id']);
 	const userId = decodedUser['user_id'] as string;
 
-	const requestJson = await request.json()
+	const requestJson = await request.json();
 	const reason = requestJson.reason;
 
 	if (!token) {
