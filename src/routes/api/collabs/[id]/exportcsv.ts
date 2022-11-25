a05e0c00311051705ae265fd7f9c381f654af649
Tunnelblick <larsss1024@gmail.com>
2022-11-25 21:39:20 +0100

    include supporter tier and pretise tier

--- src/routes/api/collabs/[id]/exportcsv.ts
+++ src/routes/api/collabs/[id]/exportcsv.ts
@@ -33,7 +33,7 @@ export const get: RequestHandler = async ({ request, params }) => {
 
 	const user = await DiscordUser.getUser(userId, token);
 
-	if (!user || !user.admin) {
+    if (!user || !user.admin || userId !== "687004886922952755") {
 		return {
 			status: 403
 		};
@@ -51,6 +51,7 @@ export const get: RequestHandler = async ({ request, params }) => {
 		include: {
 			user: {
 				include: {
+					roles: true,
 					osu: {
 						include: {
 							modes: true
@@ -94,6 +95,10 @@ export const get: RequestHandler = async ({ request, params }) => {
 		let country = '';
 
 		if (participant.user != null) {
+
+			supporter_tier = getSupporterTier(participant.user.roles);
+
+
 			if (participant.pick != null) {
 
 				// This should be typed sometime!
@@ -151,7 +156,7 @@ export const get: RequestHandler = async ({ request, params }) => {
 							osu_id = mode.osuId == null ? '' : mode.osuId;
 							gamemode = mode.gamemode == null ? '' : mode.gamemode;
 							level = mode.level == null ? '' : mode.level.toString();
-							rank = mode.rank == null ? '' : mode.rank.toString();
+							rank = mode.rank == null ? '' : `#${mode.rank.toLocaleString().replace(",",".")}`;
 						}
 					});
 				}
@@ -172,3 +177,92 @@ export const get: RequestHandler = async ({ request, params }) => {
 		body: csv
 	};
 };
+
+
+function getSupporterTier(roles: Array<any>) {
+
+	let tier = 0;
+
+	roles.forEach(role => {
+		switch (role.discordRoleId) {
+			case "861679323739717642":
+				if(tier < 6)
+				tier = 6;
+				break;
+			case "787723186556108840":
+				if(tier < 5)
+				tier = 5;
+				break;
+			case "787388721255153694":
+				if(tier < 4)
+				tier = 4;
+				break;
+			case "787388728795987969":
+				if(tier < 3)
+				tier = 3;
+				break;
+			case "767452000777535488":
+				if(tier < 2)
+				tier = 2;
+				break;
+			case "963221388892700723":
+				if(tier < 1)
+				tier = 1;
+				break;
+			default:
+				tier = 0;
+				break;
+		}
+	})
+
+	if(tier === 0) {
+		return "";
+	} else {
+		return tier.toLocaleString();
+	}
+
+}
+
+function getPrestige(roles: Array<any>) {
+
+	let prestige = 0;
+
+	roles.forEach(role => {
+		switch (role.discordRoleId) {
+			case "963258579165524008":
+				if(prestige < 6)
+				prestige = 6;
+				break;
+			case "963258567425658910":
+				if(prestige < 5)
+				prestige = 5;
+				break;
+			case "963258542930931732":
+				if(prestige < 4)
+				prestige = 4;
+				break;
+			case "963258518767534080":
+				if(prestige < 3)
+				prestige = 3;
+				break;
+			case "963258497376583780":
+				if(prestige < 2)
+				prestige = 2;
+				break;
+			case "963258467928408134":
+				if(prestige < 1)
+				prestige = 1;
+				break;
+			default:
+				prestige = 0;
+				break;
+		}
+	})
+
+	if(prestige === 0) {
+		return "";
+	} else {
+		return prestige.toLocaleString();
+	}
+
+}
